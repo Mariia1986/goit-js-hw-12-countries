dcabfab66528fca0c35a65db7d970c0d18df09e7
Mariia Randarevych <renij1986@gmail.com>
2021-01-03 19:22:29 +0300

    последние правки

--- src/index.js
+++ src/index.js
@@ -3,22 +3,40 @@ import debounce from 'lodash.debounce';
 import fetchCountries from './fetchCountries.js';
 import notifError from './notification.js';
 import countryTpl from './tamplates/template.hbs';
+import listcountry from './tamplates/listcountry.hbs';
 
 const refs = {
   input: document.querySelector('.country'),
   countryBox: document.querySelector('.countrycount'),
+  countryLink: document.querySelector('.country_link'),
 };
 
 refs.input.addEventListener('input', debounce(search, 500));
 refs.input.addEventListener('click', refreshCountries);
+refs.countryBox.addEventListener('click', onFindcountry);
 function search(e) {
   const name = e.target.value;
-if(!name){
-  refs.countryBox.innerHTML = '';
-  return
-}
+  if (!name) {
+    refs.countryBox.innerHTML = '';
+    return;
+  }
   fetchCountries(name)
-    .then(renderCountries)
+    .then(arr => {
+      if (arr.length > 1 && arr.length < 10) {
+        rendercountrylist(arr);
+      } else {
+        if (arr.length === 1) {
+          renderCountries(arr);
+        }
+        if (arr.length > 10) {
+          notifError(
+            'Превышен результат поиска',
+            'Сделайте более точный запрос',
+          );
+          refreshCountries();
+        }
+      }
+    })
     .catch(err => {
       if (err === 404) {
         notifError('Ошибка', 'Страны с таким именем не существует');
@@ -27,14 +45,17 @@ if(!name){
 }
 function renderCountries(country) {
   const markup = countryTpl(country);
+
   refs.countryBox.innerHTML = markup;
-  if (country.length > 10) {
-    notifError('Превышен результат поиска', 'Сделайте более точный запрос');
-    refreshCountries();
-  }
 }
 
 function refreshCountries(e) {
-  refs.countryBox.innerHTML = '';
   e.target.value = '';
+}
+function rendercountrylist(country) {
+  const onecountrytamp = listcountry(country);
+  refs.countryBox.innerHTML = onecountrytamp;
+}
+function onFindcountry(e) {
+  fetchCountries(e.target.textContent).then(renderCountries);
 }
